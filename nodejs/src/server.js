6de6e0704d82cf38f5e50712dc43796d3d0c5e71
Sam Xu <saxu@microsoft.com>
2015-04-20 23:47:52 +0800

    can list all the files in the folder

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -6,11 +6,14 @@ function start(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		
 		console.log("Request for " + pathname + " received.");
+		
+		route(handle, pathname, response);
+		/*
 		var content = route(handle, pathname);
 		
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(content);
-		response.end();
+		response.end();*/
 	}
 	
 	http.createServer(onRequest).listen(8888);
