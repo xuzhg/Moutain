ee3b8aa5f4cf68c4f6bb68f447d34d6930e0a877
Sam Xu <saxu@microsoft.com>
2015-04-21 00:08:05 +0800

    can send/receive data

--- nodejs/src/requestHandler.js
+++ nodejs/src/requestHandler.js
@@ -1,6 +1,7 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
 	console.log("Request handler 'start' was called.");
 	/*
 	exec("dir", function(error, stdout, stderr) {
@@ -16,7 +17,7 @@ function start(response) {
 		'charset=UTF-8" />' +
 		'</head>'+
 		'<body>'+
-		'<form actoin="/upload" method="post">'+
+		'<form action="/upload" method="post">'+
 		'<textarea name="text" rows="20" cols="60"></textarea>'+
 		'<input type="submit" value="Submit text" />'+
 		'</form>'+
@@ -28,10 +29,10 @@ function start(response) {
 	response.end();	
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+	response.write("You've sent the text: " + querystring.parse(postData).text);
 	response.end();
 }
 
--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -4,10 +4,21 @@ var url = require("url");
 function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
-		
+		var postData = "";
 		console.log("Request for " + pathname + " received.");
 		
-		route(handle, pathname, response);
+		request.setEncoding("utf8");
+		
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" + postDataChunk + "'." );
+		});
+		
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
+		
+		//route(handle, pathname, response);
 		/*
 		var content = route(handle, pathname);
 		
