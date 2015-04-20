daed89b12597a5e4c154d5f89ca7d4946b6688a2
Sam Xu <saxu@microsoft.com>
2015-04-20 23:58:45 +0800

    add a page

--- nodejs/src/requestHandler.js
+++ nodejs/src/requestHandler.js
@@ -2,11 +2,30 @@ var exec = require("child_process").exec;
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
+	/*
 	exec("dir", function(error, stdout, stderr) {
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(stdout);
 		response.end();
-	});
+	});	
+	*/
+		
+	var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Context-Type" content="text/html; '+
+		'charset=UTF-8" />' +
+		'</head>'+
+		'<body>'+
+		'<form actoin="/upload" method="post">'+
+		'<textarea name="text" rows="20" cols="60"></textarea>'+
+		'<input type="submit" value="Submit text" />'+
+		'</form>'+
+		'</body>'+
+		'</html>';
+		
+	response.writeHead(200, {"Context-Type": "text/html"});
+	response.write(body);
+	response.end();	
 }
 
 function upload(response) {
