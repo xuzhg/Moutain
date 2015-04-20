854bb62767244a1a88712d13cfb058ccd5b3aeae
Sam Xu <saxu@microsoft.com>
2015-04-20 23:10:27 +0800

    change to export

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,5 +1,20 @@
 var http = require("http");
 
+function start() {
+	function onRequest(request, response) {
+		console.log("Request received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+	
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;
+
+/*
 http.createServer(function(req, res){
 	console.log("Request received.")
 	res.writeHead(200, {"Content-Type": "text/plain"});
@@ -8,3 +23,4 @@ http.createServer(function(req, res){
 }).listen(8888);
 
 console.log("Server has started.")
+*/
