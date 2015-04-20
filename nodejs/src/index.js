ebaf23e5b0e82aecf63d43c7b851239731422493
Sam Xu <saxu@microsoft.com>
2015-04-20 23:29:47 +0800

    add request handler

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,4 +1,10 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandler.js");
 
-server.start(router.route);
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
