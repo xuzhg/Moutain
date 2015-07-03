b21874b45da21b95c2470a2fb434cdf3b4848085
Sam Xu <saxu@microsoft.com>
2015-07-03 00:09:27 -0700

    add a csharp sample

---
+++ csharp/LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToBase64();
+        }
+
+        private static void ToBase64()
+        {
+            byte[] myBytes = {1, 2, 4};
+            string base64 = Convert.ToBase64String(myBytes);
+
+            Console.WriteLine(base64);
+        }
+    }
+}
--- nodejs/src/requestHandler.js
+++ nodejs/src/requestHandler.js
@@ -58,7 +58,7 @@ function upload(response, request) {
 
 function show(response) {
 	console.log("Request handler 'show' was called.");
-	fs.readFile("./tmp/test.png", "binary", function(error, file) {
+	fs.readFile("./tmp/02-01-customer-order.png", "binary", function(error, file) {
 		if(error) {
 			response.writeHead(500, {"Content-Type":"text/plain"});
 			response.write(error + "\n");
