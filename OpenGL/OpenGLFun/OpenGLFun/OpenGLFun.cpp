275e89ee1e91fd22fdaa4ed9ea62c959267fa113
Sam Xu <saxu@microsoft.com>
2016-08-24 23:30:47 +0800

    draw a colorful triangle

--- OpenGL/OpenGLFun/OpenGLFun/OpenGLFun.cpp
+++ OpenGL/OpenGLFun/OpenGLFun/OpenGLFun.cpp
@@ -40,9 +40,13 @@ void DrawSolidTriangle()
 {
 	glBegin(GL_TRIANGLES);
 
-	glColor3f(1.0f, 1.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex2f(0.1f, 0.6f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
 	glVertex2f(0.7f, 0.6f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
 	glVertex2f(0.7f, 0.1f);
 	glEnd();
 }
