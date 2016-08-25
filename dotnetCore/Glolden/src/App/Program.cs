90c018f561e69fbc02cf42149a7f2d9db3fbe044
Sam Xu <saxu@microsoft.com>
2016-08-25 16:55:41 +0800

    add a console application for .net core

---
+++ dotnetCore/Glolden/src/App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library;
+
+namespace App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(42)}");
+        }
+    }
+}
