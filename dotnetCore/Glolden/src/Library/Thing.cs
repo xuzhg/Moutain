c69784c8bd744af2ffa313ee500871491df47fb5
Sam Xu <saxu@microsoft.com>
2016-08-25 16:49:14 +0800

    Getting started with .NET Core using Visual Studio 2015.

---
+++ dotnetCore/Glolden/src/Library/Thing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Thing
+    {
+        public int Get(int number) =>
+            Newtonsoft.Json.JsonConvert.DeserializeObject<int>($"{number}");
+    }
+}
---
+++ dotnetCore/Glolden/test/TestLibrary/LibraryTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library;
+using Xunit;
+
+namespace TestLibrary
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void ThingGetsObjectValFromNumber()
+        {
+            Assert.Equal(42, new Thing().Get(42));
+        }
+    }
+}
