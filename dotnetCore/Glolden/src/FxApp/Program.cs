58c41624602a858b656dbd764b1db085a3741b1d
Sam Xu <saxu@microsoft.com>
2016-08-25 17:02:54 +0800

    Move from 1.6 to 1.4 and add .Net framework app to test

---
+++ dotnetCore/Glolden/src/FxApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace FxApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(42)} from .NET Framework.");
+        }
+    }
+}
