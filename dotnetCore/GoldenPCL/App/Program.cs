404209a48dfe8372c28a5509fcd3725cdc37d38a
Sam Xu <saxu@microsoft.com>
2016-08-25 17:16:48 +0800

    Convert .NET fx tp .NET core

---
+++ dotnetCore/GoldenPCL/App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCLLibrary;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(42)}.");
+        }
+    }
+}
---
+++ dotnetCore/GoldenPCL/PCLLibrary/Thing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCLLibrary
+{
+    public class Thing
+    {
+        public int Get(int number) => Newtonsoft.Json.JsonConvert.DeserializeObject<int>($"{number}");
+    }
+}
