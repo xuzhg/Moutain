d5c444e1f2102803c16c26473af1a2c2bd0c8fe6
Sam Xu <saxu@microsoft.com>
2016-08-26 11:17:31 +0800

    Add a fibonacci number test

--- dotnetCore/clisample/Program.cs
+++ dotnetCore/clisample/Program.cs
@@ -4,9 +4,31 @@ namespace ConsoleApplication
 {
     public class Program
     {
+		public static int FibonacciNumber(int n)
+		{
+			int a = 0;
+			int b = 1;
+			int tmp;
+			
+			for(int i = 0; i < n; i++)
+			{
+				tmp = a;
+				a = b;
+				b += tmp;
+			}
+			
+			return a;
+		}
+		
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+			Console.WriteLine("Fibonacci numers 1-15:");
+			
+			for(int i = 0; i < 15; i++)
+			{
+				Console.WriteLine($"{i+1}: {FibonacciNumber(i)}");
+			}
         }
     }
 }
