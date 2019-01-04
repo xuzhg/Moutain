0ac6c34da5142f3584c10e939cf3910912d71d47
Sam Xu <saxu@microsoft.com>
2019-01-04 14:55:05 -0800

    Add the SportsStore MVC 5 projects

---
+++ ProMVC/SportsStore/SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6bbe7689-ab55-43f3-a5b3-912bf89e87c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProMVC/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productsRepository)
+        {
+            repository = productsRepository;
+        }
+
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
