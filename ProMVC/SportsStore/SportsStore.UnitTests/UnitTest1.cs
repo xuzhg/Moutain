f37927472b90367bb2485aac97a211e970216131
Sam Xu <saxu@microsoft.com>
2019-01-05 17:47:46 -0800

    add the navigation for the page link

--- ProMVC/SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ ProMVC/SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.HtmlHelpers;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
@@ -30,13 +33,70 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             // Act
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            //IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
 
             // Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "p4");
             Assert.AreEqual(prodArray[1].Name, "p5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange - define an HTML helper - we need to do this
+            // in order to apply the extension method
+            HtmlHelper myHelper = null;
+
+            // Arrange - create page info data
+            PageInfo pageInfo = new PageInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - set up the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pageInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Page3"">3</a>", result.ToString());
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                 new Product{ProductID = 1, Name = "p1"},
+                new Product{ProductID = 2, Name = "p2"},
+                new Product{ProductID = 3, Name = "p3"},
+                new Product{ProductID = 4, Name = "p4"},
+                new Product{ProductID = 5, Name = "p5"},
+            });
+
+            // Arrange
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            // Assert
+            PageInfo pageInfo = result.PageInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
