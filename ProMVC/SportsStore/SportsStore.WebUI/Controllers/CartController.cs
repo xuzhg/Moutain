a105ad4d106f4474bf11ccd122991cd34a22c358
Sam Xu <saxu@microsoft.com>
2019-01-06 16:15:34 -0800

    Add cart controller and index view

---
+++ ProMVC/SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductsRepository repository;
+        public CartController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        // GET: Cart
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product produc = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (produc != null)
+            {
+                GetCart().AddItem(produc, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
