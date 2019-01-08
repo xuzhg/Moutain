7e75774222927501d548f00fff679240fea7afd7
Sam Xu <saxu@microsoft.com>
2019-01-08 09:59:26 -0800

    Add Images

---
+++ ProMVC/SportsStore/SportsStore.UnitTests/ImageTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class ImageTests
+    {
+        [TestMethod]
+        public void Can_Retrieve_Image_Data()
+        {
+            // Arrange
+            Product prod = new Product
+            {
+                ProductID = 2,
+                Name = "Test",
+                ImageData = new byte[] { },
+                ImageMimeType = "image/png"
+            };
+
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product{ProductID = 1, Name = "p1"},
+                prod,
+                new Product{ProductID = 3, Name = "p3"},
+            }.AsQueryable());
+
+            // Arrange
+            ProductController target = new ProductController(mock.Object);
+
+            // Act
+            ActionResult result = target.GetImage(2);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(FileResult));
+            Assert.AreEqual(prod.ImageMimeType, ((FileResult)result).ContentType);
+        }
+
+        [TestMethod]
+        public void Cannot_Retrieve_Image_Data_For_Invalid_ID()
+        {
+            // Arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product{ProductID = 1, Name = "p1"},
+                new Product{ProductID = 2, Name = "p2"},
+            }.AsQueryable());
+
+            // Arrange
+            ProductController target = new ProductController(mock.Object);
+
+            // Act
+            ActionResult result = target.GetImage(100);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
